ee6d0c515e865b68b3e118430c98a4aebaf69376
Abisheik A <abisheik1041@gmail.com>
2020-08-04 21:53:48 +0530

    updated forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,26 +1,27 @@
 const request = require('postman-request')
 
 
-const forecast = (lat,long, callback) => { 
-    const url = "http://api.weatherstack.com/current?access_key=15e2866b28566203dad1dd4a221a42f3&query="+lat+","+long
+const forecast = (lat, long, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=15e2866b28566203dad1dd4a221a42f3&query=" + lat + "," + long
 
-    request({ url: url, json: true }, (error, response) => { 
+    request({ url: url, json: true }, (error, response) => {
         if (error) {
-            callback(undefined,"unable to connect to weather service")
+            callback(undefined, "unable to connect to weather service")
         }
         else if (response.body.error) {
-            callback(undefined,"unable to find the location")
+            callback(undefined, "unable to find the location")
         }
-        else { 
-            callback(undefined, 
-                response.body.current.weather_descriptions[0]+" .It is currently "+
-             response.body.current.temperature+" degrees out."+" It feels like "+
-             response.body.current.feelslike)+" degrees"
+        else {
+            callback(undefined,
+                response.body.current.weather_descriptions[0] + " .It is currently " +
+                response.body.current.temperature + " degrees out." + " It feels like " +
+                response.body.current.feelslike + " degrees out." + " The humidity is " +
+                response.body.current.humidity + "%.")
         }
     })
 }
 
-module.exports=forecast
+module.exports = forecast
 
 
 
